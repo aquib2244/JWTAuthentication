9405985340e481ed1df64b1e5c43fbff7e8bf692
Aquib Manzar <aquib.manzar@accenture.com>
2024-03-19 11:59:32 +0530

    Initial comment

---
+++ JWTAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+       public IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Auth([FromBody] User user)
+        {
+            IActionResult response = Unauthorized();
+
+            if(user != null)
+            {
+                if(user.UserName.Equals("test@email.com") && user.Password.Equals("a"))
+                {
+                    var issuer = _configuration["Jwt:Issuer"];
+                    var audience = _configuration["Jwt:Audience"];
+                    var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]);
+                    var signcred = new SigningCredentials(new SymmetricSecurityKey(key),
+                        SecurityAlgorithms.HmacSha512Signature);
+
+                    var subject = new ClaimsIdentity(new[] 
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                        new Claim(JwtRegisteredClaimNames.Email,user.UserName)
+                    });
+
+                    var expire = DateTime.UtcNow.AddMinutes(10);
+
+                    var tokenDescriptor = new SecurityTokenDescriptor {
+                        Subject = subject,
+                        Expires = expire,
+                        Issuer =issuer,
+                        Audience = audience,
+                        SigningCredentials = signcred
+                    };
+
+                    var tokenHandller = new JwtSecurityTokenHandler();
+                    var token = tokenHandller.CreateToken(tokenDescriptor);
+                    var jwtToken = tokenHandller.WriteToken(token);
+
+                    return Ok(jwtToken);
+
+                }
+            }
+            return response;
+        }
+    }
+}
---
+++ JWTAuthentication/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+builder.Services.AddAuthorization();
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+IConfiguration configuration = app.Configuration;
+IWebHostEnvironment environment = app.Environment;
+app.MapControllers();
+app.Run();
